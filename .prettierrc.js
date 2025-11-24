defd68f5cc706e221298ea2b36336dc34a27bc72
maxhash.io Dev Team <245432896+mine-at@users.noreply.github.com>
2025-11-24 11:06:18 -0500

    Add EditorConfig, update .gitignore, and create Prettier configuration

---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  overrides: [
+    {
+      files: ["*.html"],
+      options: {
+        tabWidth: 2,
+        useTabs: false,
+        printWidth: 120,
+        singleQuote: false,
+        bracketSpacing: true,
+        htmlWhitespaceSensitivity: "css",
+      },
+    },
+  ],
+};
